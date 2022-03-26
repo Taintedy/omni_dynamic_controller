39a65b3616ea5999e5369f07d081308a8d7eaa97
SashaKvadricepsBedra <40818765+SashaKvadricepsBedra@users.noreply.github.com>
2022-03-26 19:56:52 +0300

    lqr

--- scripts/controller_node.py
+++ scripts/controller_node.py
@@ -7,6 +7,7 @@ from geometry_msgs.msg import PoseWithCovarianceStamped
 from tf.transformations import euler_from_quaternion
 
 RATE = 100
+RATE_control = 100
 
 
 def wrap_angle(angle):
@@ -21,6 +22,7 @@ class ControllerNode(object):
         rospy.Subscriber("/secondary_robot/stm/response", String, self.callback_response, queue_size=1)
         rospy.Subscriber("/secondary_robot/filtered_coords", PoseWithCovarianceStamped, self.callback_localization, queue_size=1)
         self.timer = rospy.Timer(rospy.Duration(1. / RATE), self.tcallback_speed)
+        self.timer_control = rospy.Timer(rospy.Duration(1. / RATE_control), self.tcallback_control)
 
         # Creating trajectory
         # keypoints for the trajectory
@@ -51,10 +53,32 @@ class ControllerNode(object):
                  self.trajectory[2, i + 1] - self.trajectory[2, i]]))
         self.deltas = np.array(delta)
 
+        # Current goal
+        self.x_goal = 1
+        self.y_goal = 1
+        self.theta_goal = 1
+
         # Integrals initialization
-        self.x_int = 0  # Возможно нужно перевести все в локальную систему координат либо изменить начальные условия на текущую координату
-        self.y_int = 0
-        self.theta_int = 0
+        self.x_err_int = 0  # Возможно нужно перевести все в локальную систему координат либо изменить начальные условия на текущую координату
+        self.y_err_int = 0
+        self.theta_err_int = 0
+
+        # Initialize augmented LQR
+        self.K_fsfb = np.genfromtxt('K_fsf.csv', delimiter=',')
+        self.K_int = np.genfromtxt('K_int.csv', delimiter=',')
+
+    def tcallback_control(self, event):
+        # Defines robot state at each time step
+        self.X = np.array([self.x, self.y, self.theta, self.v, self.vn, self.w, self.i_out_0, self.i_out_1, self.i_out_2]).T
+
+        # Calculate control vector
+        Control_Int_Part = self.K_int @ self.Int_Err 
+        Control_FSFB_Part = self.K_fsfb @ self.X
+        Control_Vector = Control_Int_Part - Control_FSFB_Part
+
+        # Publish data to STM
+        control_string = "code 1" + "code 1" + "%s" % (Control_Vector.T[0])
+        self.pub_stm_command(control_string)
 
     def callback_response(self, data):
         rospy.logwarn(data.data)
@@ -69,22 +93,15 @@ class ControllerNode(object):
         #self.i_out_2 =
 
         # Publish data to STM
-        #self.i_0 = # Control current for Motor_0
-        #self.i_1 = # Control current for Motor_1
-        #self.i_2 = # Control current for Motor_2
 
     def tcallback_speed(self, event):
         data = "0 9"
         self.pub_stm_command.publish(data)
 
-    def LQR(self):
-        self.K_fsfb = np.genfromtxt('K_fsf.csv', delimiter=',')
-        self.K_int = np.genfromtxt('K_int.csv', delimiter=',')
-
     def callback_localization(self, data):
         q = [data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z,
              data.pose.pose.orientation.w]
-        self.angle = wrap_angle(euler_from_quaternion(q)[2] % (2 * np.pi))
+        angle = wrap_angle(euler_from_quaternion(q)[2] % (2 * np.pi))
         self.robot_pose = np.array(
             [data.header.stamp.to_sec(), data.pose.pose.position.x, data.pose.pose.position.y, wrap_angle(angle)])
         rospy.logwarn(self.robot_pose)
@@ -94,23 +111,25 @@ class ControllerNode(object):
         self.y = self.pose.pose.position.y  # Position Y
         self.theta = self.angle             # Angle Theta
 
-    def integrals(self):
+        self.integral_errors() # call integral_errors after receiving new message 
+
+    def integral_errors(self):
+        # Calculate deviation between goal and current measurement
+        x_dev_err = self.x_goal - self.x
+        y_dev_err = self.y_goal - self.y
+        theta_dev_err = self.theta_goal - self.theta
         # Integrals Data
-        self.x_err_int += self.x                # Intergal of Position X
-        self.y_err_int += self.y                # Intergal of Position Y
-        self.theta_err_int += self.theta        # Intergal of Angle Theta
-
-    def robot_state(self):
-        # Defines robot state at each time step
-        self.X = np.array([self.x, self.y, self.theta, self.v, self.vn, self.w, self.i_out_0, self.i_out_1, self.i_out_2, self.x_int, self.y_int, self.theta_int])
-
-    def deviation_errors(self):
-        #self.x_dev_err =
-        #self.y_dev_err =
-        #self.theta_dev_err =
+        self.x_err_int += x_dev_err                # Intergal of Position Error X
+        self.y_err_int += y_dev_err                # Intergal of Position Error Y
+        self.theta_err_int += theta_dev_err        # Intergal of Angle Error Theta
+        # Assign column vector of integral errors  
+        self.Int_Err = np.array([[self.x_err_int],      
+                                 [self.y_err_int],
+                                 [self.theta_err_int]])
+        
+    
 def shutdown():
     rospy.logwarn("Controller shutting down")
-
 
 if __name__ == '__main__':
     try:
