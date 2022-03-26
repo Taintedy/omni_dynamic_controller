29042e520973a0b2a6fe5b0d0e4af5a47c34cae4
Taintedy <merly.miky@gmail.com>
2022-03-26 17:35:05 +0300

    added pf coords subscriber

--- scripts/controller_node.py
+++ scripts/controller_node.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 import warnings
+import numpy as np
 import rospy
 from std_msgs.msg import String
-from geometry_msgs.msg import TransformStamped, PoseArray, Pose, Quaternion, PoseStamped, PoseWithCovarianceStamped, \
-    Pose2D
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from tf.transformations import euler_from_quaternion
 
 RATE = 40
 
+def wrap_angle(angle):
+
+    return (angle + np.pi) % (2 * np.pi) - np.pi
 
 class ControllerNode(object):
     def __init__(self):
-        self.pub_stm_command = rospy.Publisher("stm/command", String, queue_size=1)
+        self.robot_pose = np.zeros(4)
+
+        self.pub_stm_command = rospy.Publisher("/secondary_robot/stm/command", String, queue_size=1)
         rospy.Subscriber("/secondary_robot/stm/response", String, self.callback_response, queue_size=1)
-        rospy.Subscriber("/secondary_robot/filtered_coords", PoseWithCovarianceStamped, self.callback_response, queue_size=1)
+        rospy.Subscriber("/secondary_robot/filtered_coords", PoseWithCovarianceStamped, self.callback_localization, queue_size=1)
         self.timer = rospy.Timer(rospy.Duration(1. / RATE), self.tcallback_speed)
 
     def callback_response(self, data):
@@ -21,6 +27,14 @@ class ControllerNode(object):
     def tcallback_speed(self, event):
         data = "0 9"
         self.pub_stm_command.publish(data)
+
+    def callback_localization(self, data):
+        q = [data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z,
+             data.pose.pose.orientation.w]
+        angle = wrap_angle(euler_from_quaternion(q)[2] % (2 * np.pi))
+        self.robot_pose = np.array(
+            [data.header.stamp.to_sec(), data.pose.pose.position.x, data.pose.pose.position.y, wrap_angle(angle)])
+        rospy.logwarn(self.robot_pose)
 
 
 def shutdown():
