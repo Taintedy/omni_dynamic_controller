93c6b519ae59208da89300f7366aeb555da28fba
Taintedy <merly.miky@gmail.com>
2022-03-26 18:43:04 +0300

    added trajectory

--- scripts/controller_node.py
+++ scripts/controller_node.py
@@ -6,7 +6,11 @@ from std_msgs.msg import String
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from tf.transformations import euler_from_quaternion
 
-RATE = 40
+RATE = 100
+
+
+
+
 
 def wrap_angle(angle):
 
@@ -21,8 +25,33 @@ class ControllerNode(object):
         rospy.Subscriber("/secondary_robot/filtered_coords", PoseWithCovarianceStamped, self.callback_localization, queue_size=1)
         self.timer = rospy.Timer(rospy.Duration(1. / RATE), self.tcallback_speed)
 
+        poses = np.array([[1, 0, 0],
+                          [1, 0.5, np.pi / 2],
+                          [0, 0, np.pi]])
+        x = np.array([0])
+        y = np.array([0])
+        theta = np.array([0])
+        for pose in poses:
+            x_lin = np.linspace(x[-1], pose[0], 100)
+            y_lin = np.linspace(y[-1], pose[1], 100)
+            theta_lin = np.linspace(theta[-1], pose[2], 100)
+
+            x = np.concatenate([x, x_lin])
+            y = np.concatenate([y, y_lin])
+            theta = np.concatenate([theta, theta_lin])
+
+        self.trajectory = np.vstack([x, y, theta])
+        delta = []
+
+        for i in range(self.trajectory.shape[1] - 1):
+            delta.append(np.array([self.trajectory[0, i + 1] - self.trajectory[0, i], self.trajectory[1, i + 1] - self.trajectory[1, i],
+                                   self.trajectory[2, i + 1] - self.trajectory[2, i]]))
+        self.deltas = np.array(delta)
+
+
     def callback_response(self, data):
-        rospy.logwarn(data.data)
+        # rospy.logwarn(data.data)
+        pass
 
     def tcallback_speed(self, event):
         data = "0 9"
@@ -34,7 +63,7 @@ class ControllerNode(object):
         angle = wrap_angle(euler_from_quaternion(q)[2] % (2 * np.pi))
         self.robot_pose = np.array(
             [data.header.stamp.to_sec(), data.pose.pose.position.x, data.pose.pose.position.y, wrap_angle(angle)])
-        rospy.logwarn(self.robot_pose)
+        # rospy.logwarn(self.robot_pose)
 
 
 def shutdown():
