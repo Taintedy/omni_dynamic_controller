fbec92ee808ece6fb294c3d600046ce7db750bfa
Taintedy <merly.miky@gmail.com>
2022-03-28 03:24:59 +0300

    controller tests

--- scripts/controller_node.py
+++ scripts/controller_node.py
@@ -49,9 +49,9 @@ class ControllerNode(object):
         self.deltas = np.array(delta)
 
         # Current goal
-        self.x_goal = 0.1
-        self.y_goal = 0.1
-        self.theta_goal = 0.1
+        self.x_goal = 1
+        self.y_goal = 0
+        self.theta_goal = 0
 
         # Integrals initialization
         self.x_err_int = 0  # Возможно нужно перевести все в локальную систему координат либо изменить начальные условия на текущую координату
@@ -64,15 +64,19 @@ class ControllerNode(object):
         self.w = 0
 
         # initial poses
-        self.x = 2.75              # Position X
-        self.y = 0.85              # Position Y
-        self.theta = 3.14          # Position Theta
+        self.x = 0  # Position X
+        self.y = 0  # Position Y
+        self.theta = 0  # Position Theta
 
         # Initialize augmented LQR
-        self.K_fsfb = np.genfromtxt('/home/rpi2/catkin_ws/src/reset_22/omni_dynamic_controller/scripts/K_fsfb.csv', delimiter=',')
-        self.K_int = np.genfromtxt('/home/rpi2/catkin_ws/src/reset_22/omni_dynamic_controller/scripts/K_int.csv', delimiter=',')
+        self.K_fsfb = np.genfromtxt('/home/rpi2/catkin_ws/src/reset_22/omni_dynamic_controller/scripts/K_fsfb.csv',
+                                    delimiter=',')
+        self.K_int = np.genfromtxt('/home/rpi2/catkin_ws/src/reset_22/omni_dynamic_controller/scripts/K_int.csv',
+                                   delimiter=',')
 
         self.pub_stm_command = rospy.Publisher("/secondary_robot/stm/command", String, queue_size=1)
+        self.pub_robot_command = rospy.Publisher("/secondary_robot/robot_command", String, queue_size=1)
+        self.state_pub = rospy.Publisher("/secondary_robot/robot_state", String, queue_size=1)
         rospy.Subscriber("/secondary_robot/stm/response", String, self.callback_response, queue_size=1)
         rospy.Subscriber("/secondary_robot/filtered_coords", PoseWithCovarianceStamped, self.callback_localization,
                          queue_size=1)
@@ -82,27 +86,52 @@ class ControllerNode(object):
     def tcallback_control(self, event):
         # Defines robot state at each time step
         if self.Int_Err is not None:
-            self.X = np.array([self.x, self.y, self.theta, self.v, self.vn, self.w]).T
+            self.X = np.array([self.x, self.y, self.theta, self.v, self.vn, self.w])[np.newaxis].T
+            dist = np.sqrt((self.x - self.x_goal)**2 + (self.y - self.y_goal)**2)
+            if dist <= 0.1:
+                control_string = "0" + " 0x50 " + "0 0 0"
+                #rospy.logwarn("control cmd: %s" % control_string)
+                self.pub_stm_command.publish(control_string)
+                self.Int_Err = np.array([[0],
+                                         [0],
+                                         [0]])
+            else:
+                self.state_pub.publish("%s" % (self.X.T[0]))
+                # Calculate control vector
+                #rospy.logwarn('K_int %s' % self.K_int)
+                Control_Int_Part = self.K_int @ self.Int_Err
+                Control_FSFB_Part = self.K_fsfb @ self.X
+                Control_Vector = Control_Int_Part - Control_FSFB_Part
 
-            # Calculate control vector
-            Control_Int_Part = self.K_int @ self.Int_Err
-            Control_FSFB_Part = self.K_fsfb @ self.X
-            Control_Vector = Control_Int_Part - Control_FSFB_Part
+                rospy.logwarn("control_string: %s" % Control_Vector)
+                rospy.logwarn("Control_Int_Part: %s" % Control_Int_Part)
+                rospy.logwarn("Control_FSFB_Part: %s" % Control_FSFB_Part)
 
-            # Publish data to STM
-            control_string = "0" + " 50 " + "%f %f %f" % Control_Vector.T[0]
-            rospy.logwarn("control_string: %s" % control_string)
-            self.pub_stm_command.publish(control_string)
+                for i in range(3):
+                    if abs(Control_Vector[i]) > 0.5:
+                        Control_Vector[i] = np.sign(Control_Vector[i]) * 0.5
+
+                # Publish data to STM
+                control_string = "%f %f %f" % (
+                Control_Vector.T[0][0], Control_Vector.T[0][1], Control_Vector.T[0][2])
+                rospy.logwarn("control cmd: %s" % control_string)
+
+                self.pub_robot_command.publish(control_string)
 
     def callback_response(self, data):
-        rospy.logwarn("speed data: %s" % data.data)
+        #rospy.logwarn("speed data: %s" % data.data)
+        splitted_data = data.data.split()
         # Subscribe to STM data
         # Kinematics Data Parse
-        speed_vals = [float(token) for token in data.data.split()]
-        self.v = speed_vals[1]           # Forward Speed V
-        self.vn = speed_vals[2]          # Lateral Speed Vn
-        self.w = speed_vals[3]           # Angular Speed W
 
+        if len(splitted_data) == 4:
+            speed_vals = [float(token) for token in splitted_data]
+            self.v = speed_vals[1]  # Forward Speed V
+            self.vn = speed_vals[2]  # Lateral Speed Vn
+            self.w = speed_vals[3]  # Angular Speed W
+        else:
+            pass
+            #rospy.logwarn("not speed value")
 
     def tcallback_speed(self, event):
         data = "0 9"
@@ -114,14 +143,14 @@ class ControllerNode(object):
         angle = wrap_angle(euler_from_quaternion(q)[2] % (2 * np.pi))
         self.robot_pose = np.array(
             [data.header.stamp.to_sec(), data.pose.pose.position.x, data.pose.pose.position.y, wrap_angle(angle)])
-        rospy.logwarn("robot pose: %s" % self.robot_pose)
+        # rospy.logwarn("robot pose: %s" % self.robot_pose)
 
         # Localization Data Parse
-        self.x = self.robot_pose[1]         # Position X
-        self.y = self.robot_pose[2]         # Position Y
-        self.theta = self.robot_pose[3]     # Angle Theta
+        self.x = 2.75 - self.robot_pose[1]  # Position X
+        self.y = 0.85 - self.robot_pose[2]  # Position Y
+        self.theta = wrap_angle(3.14 - self.robot_pose[3])  # Angle Theta
 
-        self.integral_errors() # call integral_errors after receiving new message 
+        self.integral_errors()  # call integral_errors after receiving new message
 
     def integral_errors(self):
         # Calculate deviation between goal and current measurement
@@ -129,17 +158,20 @@ class ControllerNode(object):
         y_dev_err = self.y_goal - self.y
         theta_dev_err = self.theta_goal - self.theta
         # Integrals Data
-        self.x_err_int += x_dev_err                # Intergal of Position Error X
-        self.y_err_int += y_dev_err                # Intergal of Position Error Y
-        self.theta_err_int += theta_dev_err        # Intergal of Angle Error Theta
+        self.x_err_int += x_dev_err  # Intergal of Position Error X
+        self.y_err_int += y_dev_err  # Intergal of Position Error Y
+        self.theta_err_int += theta_dev_err  # Intergal of Angle Error Theta
         # Assign column vector of integral errors  
-        self.Int_Err = np.array([[self.x_err_int],      
+        self.Int_Err = np.array([[self.x_err_int],
                                  [self.y_err_int],
                                  [self.theta_err_int]])
-        
-    
+
+
 def shutdown():
+    control_string = "0" + " 0x50 " + "0 0 0"
+    controller_node.pub_stm_command.publish(control_string)
     rospy.logwarn("Controller shutting down")
+
 
 if __name__ == '__main__':
     try:
