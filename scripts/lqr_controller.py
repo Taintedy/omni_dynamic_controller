31d0d7693856fd94bcfaf6c91f9f1f89bca05ae4
Taintedy <merly.miky@gmail.com>
2022-05-23 17:54:21 +0300

    Working refactored lqr

--- scripts/lqr_controller.py
+++ scripts/lqr_controller.py
@@ -5,39 +5,27 @@ import rospy
 from std_msgs.msg import String, Header
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from tf.transformations import euler_from_quaternion
-from nav_msgs.msg import Odometry
 import tf2_ros
 
-# rospy.logwarn("Hello world!")
-# Define control rate
 RATE_get_local_robot_speed = 50
 RATE_publish_control = 50
 
 
-def cvt_global2local(global_point, src_point):
-    size = global_point.shape[-1]
-    x1, y1, a1 = 0, 0, 0
-    if size == 3:
-        x1, y1, a1 = global_point.T
-    elif size == 2:
-        x1, y1 = global_point.T
-    X, Y, A = src_point.T
-    x = x1 * np.cos(A) + y1 * np.sin(A) - X * np.cos(A) - Y * np.sin(A)
-    y = -x1 * np.sin(A) + y1 * np.cos(A) + X * np.sin(A) - Y * np.cos(A)
-    a = (a1 - A + np.pi) % (2 * np.pi) - np.pi
-    if size == 3:
-        return np.array([x, y, a]).T
-    elif size == 2:
-        return np.array([x, y]).T
-    else:
-        return
-
-
 def wrap_angle(angle):
+    """
+    Wraps the given angle to the range [-pi, +pi].
+    :param angle: The angle (in rad) to wrap (can be unbounded).
+    :return: The wrapped angle (guaranteed to in [-pi, +pi]).
+    """
     return (angle + np.pi) % (2 * np.pi) - np.pi
 
 
 def unfold_angles(angles):
+    """
+    Unfolds the angle array to be >= +- pi
+    :param angle: An array of angles.
+    :return: Array of unwraped angles.
+    """
     angles = wrap_angle(angles)
     delta = angles[1:] - angles[:-1]
     delta = np.where(delta > np.pi, delta - 2 * np.pi, delta)
@@ -46,15 +34,11 @@ def unfold_angles(angles):
 
 
 class ControllerNode(object):
-    """
-    Controller operates in the local coordinate system assigned with the robot.
-    Localization coordinates should be transformed into local robot coordinates.
-    Target point should be transformed into local coordinate system.
-    """
 
     def __init__(self):
         self.robot_state = np.zeros(6)
         self.angle_array = []  # Angle array to unfold angle
+        self.flag = False  # End of path flag
 
         # tf listener for localization
         self.tfBuffer = tf2_ros.Buffer()
@@ -64,7 +48,7 @@ class ControllerNode(object):
         angle = np.linspace(np.pi, 3 * np.pi, 150)
         x = (0.5 * 2 ** 0.5 * np.cos(angle)) / (1 + np.sin(angle) ** 2) + 1.5
         y = (0.5 * 2 ** 0.5 * np.cos(angle) * np.sin(angle)) / (1 + np.sin(angle) ** 2)
-        self.path = np.array([x, y, 10 * angle]).T
+        self.path = np.array([x, y, angle]).T
         self.path = np.append(self.path, [[self.path[0, 0], self.path[0, 1], self.path[-1, 2]]], axis=0)
         self.path = np.append(self.path, [[0, 0, self.path[-1, 2]]], axis=0)
 
@@ -79,8 +63,6 @@ class ControllerNode(object):
 
         # Creating ROS Subscribers
         rospy.Subscriber("/secondary_robot/stm/response", String, self.callback_response, queue_size=1)
-        rospy.Subscriber("/secondary_robot/filtered_coords", PoseWithCovarianceStamped, self.callback_localization,
-                         queue_size=1)
 
         # Initialize poll timers
         self.timer = rospy.Timer(rospy.Duration(1. / RATE_get_local_robot_speed), self.tcallback_speed)
@@ -91,19 +73,17 @@ class ControllerNode(object):
 
         Each time the timer ticks with requested rate,
         this function:
-            1) Calculates state of the robot self.X: [x_local, y_local, theta_local, vx_local, vy_local, w_local]
-            2) It calls function to publish command for the STM to get the robot speed
-            3) It initializes Int_Err
-            4) It publishes robot state X
-            5) Calculates Control from Int part and FSFB part and gets summary control vector
-            6) Publishes control command for the STM
+            1) Calculates state of the robot X: [x, y, theta, vx, vy, w]
+            2) It publishes robot state X
+            3) Calculates Control and gets summary control vector
+            4) Publishes control command for the STM
         """
 
         pose_dev = self.calc_errors()
         if not self.flag:
             if self.is_near_goal():
                 control_string = "0" + " 0x08 " + "0 0 0"
-                rospy.logwarn("control cmd: %s" % control_string)
+                rospy.logwarn("got to goal")
                 self.pub_stm_command.publish(control_string)
 
                 if self.cnt <= self.path.shape[0]:
@@ -114,18 +94,17 @@ class ControllerNode(object):
             else:
                 state = Header()
                 state.stamp = rospy.Time.now()
-                state.frame_id = "%s %s %s %s %s %s" % (self.robot_state.T[0, 0], self.robot_state.T[0, 1], self.robot_state.T[0, 2], self.robot_state.T[0, 3], self.robot_state.T[0, 4], self.robot_state.T[0, 5])
+
+                state.frame_id = "%s %s %s %s %s %s" % (
+                    self.robot_state[0], self.robot_state[1],
+                    self.robot_state[2], self.robot_state[3],
+                    self.robot_state[4], self.robot_state[5])
+
                 self.state_pub_header.publish(state)
                 control_signal = self.get_control_signal(pose_dev)
 
-                rotation_matrix = np.array([[np.cos(self.robot_state[2]), np.sin(self.robot_state[2])],
-                                            [-np.sin(self.robot_state[2]), np.cos(self.robot_state[2])]])
-
-                control_signal = rotation_matrix @ control_signal[np.newaxis].T
-
                 # Publish data to STM
                 control_string = "%f %f %f" % (control_signal[0], control_signal[1], control_signal[2])
-
                 control_string = "0" + " 0x08 " + control_string
                 self.pub_stm_command.publish(control_string)
                 self.pub_robot_command.publish(control_string)
@@ -135,26 +114,41 @@ class ControllerNode(object):
             self.pub_stm_command.publish(control_string)
             self.flag = True
 
-
     def get_control_signal(self, deviations):
-        control_signal_vx = 0.80975 * deviations[0] * 2.5 - 0.06663 * deviations[3] * 1.5
-        control_signal_vy = 0.80971 * deviations[1] * 2.5 + 0.06163 * deviations[4] * 1.5
-        control_signal_w = 0.8094 * deviations[2] * 2.5 - 0.03775 * deviations[5] * 1.5
-        return np.array([control_signal_vx, control_signal_vy, control_signal_w])
+        """
+        Uses coefficents of the LQR controller to calculate the
+        control vector for the omni-directional platform
+        :param deviations: Array of deviations [x_dev, y_dev, theta_dev] from robot position to goal position.
+        :return: Array of control outputs [control_signal_vx, control_signal_vy, control_signal_w]
+        """
+
+        control_signal_vx = 0.80975 * deviations[0] * 1.5 + 0.06663 * deviations[3] * 1.5
+        control_signal_vy = 0.80971 * deviations[1] * 1.5 + 0.06163 * deviations[4] * 1.5
+        control_signal_w = 0.8094 * deviations[2] * 1.5 + 0.03775 * deviations[5] * 1.5
+
+        rotation_matrix = np.array([[np.cos(self.robot_state[2]), np.sin(self.robot_state[2])],
+                                    [-np.sin(self.robot_state[2]), np.cos(self.robot_state[2])]])
+        speed = np.array([[control_signal_vx],
+                          [control_signal_vy]])
+        rotated_speed = rotation_matrix @ speed
+        return np.array([rotated_speed[0, 0], rotated_speed[1, 0], control_signal_w])
 
     def is_near_goal(self):
-        dist = np.sqrt((self.robot_state[0] - self.current_goal[0]) ** 2 + (self.robot_state[1] - self.current_goal[1]) ** 2)
+        dist = np.sqrt(
+            (self.robot_state[0] - self.current_goal[0]) ** 2 + (self.robot_state[1] - self.current_goal[1]) ** 2)
         theta_err = abs(self.robot_state[2] - self.current_goal[2])
-        return dist <= 0.1 and theta_err < np.pi / 2
+        return dist <= 0.1 and theta_err < np.pi / 20
 
     def callback_response(self, data):
         """Function parses data from STM into robot local frame speed data.
+        Additionally it uses tfBuffer to get the pose of the robot
 
         Each time when data is published in topic "/secondary_robot/stm/response",
         this function is called.
 
+        All processed data is saved in the parameter self.robot_state
+
         :param data: Data from the STM topic "/secondary_robot/stm/response"
-        :return: self.vx_local, self.vy_local, self.w_local
         """
         splitted_data = data.data.split()
 
@@ -188,7 +182,12 @@ class ControllerNode(object):
         self.pub_stm_command.publish(data)  # Publish required command to the topic "/secondary_robot/stm/command"
 
     def calc_errors(self):
-        # Calculate deviation between goal and current measurement
+        """
+        Uses the robot state and goal positions to calculate the deviation values
+
+        :return error_arr: Array of deviations [x_deviation, y_deviation, theta_deviation,
+                                                vx_deviation, vy_deviation, w_deviation]
+        """
         error_arr = np.zeros(6)
         error_arr[0] = self.current_goal[0] - self.robot_state[0]
         error_arr[1] = self.current_goal[1] - self.robot_state[1]
@@ -200,6 +199,13 @@ class ControllerNode(object):
 
 
 def shutdown():
+    """
+    Function is called on shutdown.
+
+    Sends 0 control to stm to stop the robot on shutdown.
+
+    ***DOES NOT ALWAYS WORK***
+    """
     control_string = "0" + " 0x08 " + "0 0 0"
     controller_node.pub_stm_command.publish(control_string)
     rospy.logwarn("Controller shutting down")
